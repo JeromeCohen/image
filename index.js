4401a9c85100c71f9355a8ae2489980c5ecc207d
jeromecohen <jeromecohen98@gmail.com>
2019-04-24 12:17:18 -0400

    for Redhat

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cron = require('node-cron');
 const mongoose = require('mongoose');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const sendRoutes = require('./routes/send.js');
 const Message = require('./models/message.js');
 const sender = require('./sendMessages.js');
-const html = require('./views/index.html');
 const PORT = process.env.PORT || 3000;
 
 //Set up DB
@@ -27,6 +27,7 @@ cron.schedule('0 0 0 * * *', () => {
 //Routes Boilerplate
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -34,7 +35,7 @@ app.use(function(req, res, next) {
 });
 app.use('/send', sendRoutes);
 app.get('/', (req, res) => {
-	res.send('html');
+	res.sendFile('index.html');
 });
 app.listen(PORT , () => {
 	console.log(`Listening on port ${PORT}`);
