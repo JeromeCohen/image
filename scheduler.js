9d5239d95dfc9bf4b20ac670b491571be1a6e1d1
jeromecohen <jeromecohen98@gmail.com>
2019-04-25 16:29:40 -0400

    added db connection

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,12 @@
 //For Heroku scheduler instead of cron
 const sender = require('./sendMessages.js');
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('open')
+});
+
 sender.clearQueue();
